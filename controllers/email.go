43f1f4ff4aa75406eb66aacee27ece3e8bb2c320
Orhan Gazi Dizdaroğlu <orhangazid@gmail.com>
2020-09-27 14:23:17 +0300

    add parse template

--- controllers/email.go
+++ controllers/email.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"bytes"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
@@ -16,12 +18,34 @@ var (
 	Password = os.Getenv("EMAIL_PASSWORD")
 )
 
+type mailContent struct {
+	Name string
+}
+
 // Information struct for mapping
 type Information struct {
 	Name    string `json:"name"`
 	Surname string `json:"surname"`
 	Email   string `json:"email"`
 	Type    int    `json:"type"`
+}
+
+func parseMail(temp string, name string) string {
+	t := template.New("mail")
+
+	t, err := t.Parse(temp)
+	if err != nil {
+		log.Fatalf("Error while parsing template %s", err)
+	}
+	c := &mailContent{}
+	c.Name = name
+	var tpl bytes.Buffer
+	if err := t.Execute(&tpl, c); err != nil {
+		log.Println(err)
+	}
+
+	result := tpl.String()
+	return result
 }
 
 func getEmailTemplate(mailType int) (string, error) {
@@ -33,12 +57,14 @@ func getEmailTemplate(mailType int) (string, error) {
 }
 
 func send(information Information) (result Information, err error) {
-	template, _ := models.ReadTemplate(information.Type)
+	template, _ := getEmailTemplate(information.Type)
+	parsedMail := parseMail(template, information.Name)
+
 	m := gomail.NewMessage()
 	m.SetHeader("From", Username)
 	m.SetHeader("To", information.Email)
 	m.SetHeader("Subject", "Team Momentum.")
-	m.SetBody("text/html", template)
+	m.SetBody("text/html", parsedMail)
 	d := gomail.NewDialer(Host, 465, Username, Password)
 
 	if err := d.DialAndSend(m); err != nil {
